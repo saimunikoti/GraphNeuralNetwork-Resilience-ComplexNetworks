f61a0831f129a185f5350ee079f685f432f29050
Sai Munikoti <reachmsai@gmail.com>
2020-09-01 13:33:52 -0500

    Semisup file is modifed to learn on a single composite graph consisting of multiple networks of diff sizes.

--- src/Semisup_Graphsage_nodebtw.py
+++ src/Semisup_Graphsage_nodebtw.py
@@ -6,7 +6,7 @@ from src.data.make_dataset import GenEgrData
 from stellargraph import StellarGraph
 from stellargraph import mapper
 from stellargraph.mapper import GraphSAGENodeGenerator
-from stellargraph.layer import GraphSAGE, MeanPoolingAggregator, MaxPoolingAggregator
+from stellargraph.layer import GraphSAGE, MeanPoolingAggregator, MaxPoolingAggregator, MeanAggregator
 
 # from stellargraph_Custom import StellarGraph
 # from stellargraph_Custom import mapper
@@ -17,7 +17,7 @@ from stellargraph.layer import GraphSAGE, MeanPoolingAggregator, MaxPoolingAggre
 # sys.path.append(os.path.join(os.getcwd(), "keras-deep-graph-learning-master"))
 # sys.path.append(os.path.join(os.getcwd(), "keras-deep-graph-learning-master/examples")) # Adding the submodule to the module search path
 
-from tensorflow.keras import layers, optimizers, losses, metrics, Model
+from tensorflow.keras import layers, optimizers, losses, metrics, Model, models
 from tensorflow.keras.callbacks import ModelCheckpoint, Callback
 from sklearn import preprocessing, feature_extraction, model_selection
 from stellargraph import datasets
@@ -25,26 +25,48 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 import keras.backend as K
-md=GenEgrData()
+md = GenEgrData()
 import scipy.stats as stats
 from src.visualization import visualize as vs
 from src.features import build_features as bf
+import pickle
 #%% #################################### Load Dataset #########################################################
 #%% ############################################################################################################
 ## egr data
-datadir = ".\data\processed\\adj_egrclass_pl_1000n_"
-#
-predictor, label, feature, graph = md.gen_egr_plmodel(1, 4000, datadir, "adjacency", genflag=1)
-g = graph[0]
+datadir = ".\data\processed\\adj_egrclass_pl_200n_"
+listgraph=[]
+listlabel=[]
 
-nx.write_gpickle(g, ".\data\processed\\adj_egrclass_pl_4000n.gpickle")
+for graphsize in [100, 200, 300, 500, 700, 1000, 1500, 2000, 3000, 4000]:
+    predictor, label, feature, graphlist = md.gen_egr_plmodel(1, graphsize, datadir, "adjacency", genflag=1)
+    listgraph.append(graphlist[0])
+    listlabel.append(label[0,:])
 
-## load graphs
-# g = nx.read_gpickle(".\\data\processed\\Graph_2000.gpickle")
+g = bf.combine_graphs(listgraph)
+
+## save graph and corresponding labels
+nx.write_gpickle(g, ".\data\processed\\adj_egrclass_pl_13300nodes.gpickle")
+
+with open(".\data\processed\\"+"13000nodes_label.pickle", 'wb') as b:
+    pickle.dump(listlabel, b)
+
+## load graphs and labels
+g = nx.read_gpickle(".\\data\processed\\adj_egrclass_pl_13300nodes.gpickle")
+
+with open(".\data\processed\\"+"13000nodes_label.pickle", 'rb') as b:
+    listlabel = pickle.load(b)
+
+# flatten label of multi graph
+# targetlabel = label.flatten('C')
+
+targetlabel = listlabel[0]
+for countlen in range(len(listlabel)-1):
+    targetlabel = np.concatenate((targetlabel, listlabel[countlen+1]), axis=0)
 
 targetdf = pd.DataFrame()
-label = np.reshape(label, (label.shape[1],))
-targetdf['btw'] = label
+# label = np.reshape(label, (label.shape[1],))
+targetdf['btw'] = targetlabel
+# targetdf = targetdf.drop([99, 100])
 
 ## load saved egr targets of graph
 # targetdf= pd.read_excel(".\\data\processed\\egr_2000.xlsx")
@@ -69,21 +91,22 @@ train_subjects, test_subjects = model_selection.train_test_split(targetdf, train
 train_targets = np.array(train_subjects)
 test_targets = np.array(test_subjects)
 
-#%% #################################### Graphsage Model building ###########################################
+## #################################### Graphsage Model building ###########################################
 #%% ############################################################################################################
 
-batch_size = 40
-num_samples = [10, 10, 5, 5]
+batch_size = 20
+num_samples = [10, 10, 5]
 generator = GraphSAGENodeGenerator(G, batch_size, num_samples)
 
 train_gen = generator.flow(train_subjects.index, train_targets, shuffle=True)  # train_subjects.index for selecting training nodes
 test_gen = generator.flow(test_subjects.index, test_targets)
 
 # aggregatortype = MaxPoolingAggregator()
-graphsage_model = GraphSAGE(layer_sizes=[32, 32, 16, 16], generator=generator, activations=["relu","relu","relu","linear"],
+graphsage_model = GraphSAGE(layer_sizes=[32, 32, 16], generator=generator, activations=["relu","relu","linear"],
                             bias=True, dropout=0.0)
 
 x_inp, x_out = graphsage_model.in_out_tensors()
+# x_out1 = layers.Dense(units=10, activation="relu")(x_out)
 prediction = layers.Dense(units=train_targets.shape[1], activation="relu")(x_out)
 
 model = Model(inputs=x_inp, outputs=prediction)
@@ -116,24 +139,11 @@ def noderankloss(index):
     return loss
 
 ##
-model.compile( optimizer=optimizers.Adam(), loss = noderankloss(indices), metrics=["acc"])
-model.compile( optimizer=optimizers.Adam(lr=0.005), loss="mean_squared_error", metrics=["acc"])
+model.compile( optimizer=optimizers.Adam(lr=0.005), loss = noderankloss(indices), metrics=["acc"])
+model.compile( optimizer=optimizers.RMSprop(), loss="mean_squared_error", metrics=["acc"])
 
-filepath ='.\models\\EGR_Graphsage\\pl500-3000_m1test1.h5'
+filepath ='.\models\\EGR_Graphsage\\pl13000_customloss.h5'
 mcp = ModelCheckpoint(filepath, save_best_only=True, monitor='val_loss', mode='min')
 
-history = model.fit(train_gen, epochs=15, validation_data=test_gen, callbacks=[mcp], verbose=2, shuffle=False)
+history = model.fit(train_gen, epochs=50, validation_data=test_gen, callbacks=[mcp], verbose=2, shuffle=False)
 
-#%% #################################### Model Evaluation ######################################################
-#%% ############################################################################################################
-
-all_nodes = test_subjects.index
-all_mapper = generator.flow(all_nodes)
-y_pred = model.predict(all_mapper)
-
-## kendall tau metric for rank
-ktau, p_value = stats.kendalltau(test_targets, y_pred)
-print(ktau)
-
-## top k pf
-vs.compute_topkperf(test_targets, y_pred, 0.9)
