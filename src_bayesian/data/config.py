a6891df566667a0193bcc1b0384b0af7305cf066
Sai Munikoti <saimunikoti@users.campus>
2021-06-10 15:46:09 -0500

    folder rename

--- src_bayesian/data/config.py
+++
@@ -1,9 +0,0 @@
-
-projectpath = r"C:\Users\saimunikoti\Manifestation\centrality_learning"
-
-datapath = projectpath + "\\data\\"
-
-modelpath = projectpath + "\\models\\Graphsage\\"
-
-linkmodel = projectpath + "\\models\\Graphsage_link\\"
-
